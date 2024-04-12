aa52f8ebc908639a434f85166b58d9c695e350c6
thanhoaiedunlu <21130360@st.hcmuaf.edu.vn>
2024-04-12 23:34:16 +0700

    edit

--- app/src/main/java/com/example/saleapp/LoginActivity.java
+++ app/src/main/java/com/example/saleapp/LoginActivity.java
@@ -10,21 +10,33 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText email, password;
     private TextView txtRegister, notification, txtForgotPassword;
+    private View viewGoogle, viewFacebook;
     private Button btnLogin;
    private FirebaseAuth auth;
-    FirebaseDatabase database;
-    ProgressBar progressBar;
+    private FirebaseDatabase database;
+    private ProgressBar progressBar;
+    private GoogleSignInClient client;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,10 +47,19 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.btn_login);
         notification = findViewById(R.id.tv_notification);
         txtForgotPassword = findViewById(R.id.tv_forgotPassword);
+        viewGoogle = findViewById(R.id.google);
+        viewFacebook = findViewById(R.id.facebook);
         progressBar = findViewById(R.id.processbar);
         progressBar.setVisibility(View.GONE);
         auth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
+
+        GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        client = GoogleSignIn.getClient(this, options);
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,8 +81,42 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        viewGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = client.getSignInIntent();
+                startActivityForResult(intent, 1234);
+
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1234){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+                FirebaseAuth.getInstance().signInWithCredential(credential)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()){
+                                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                                    startActivity(intent);
+                                }else {
+                                    notification.setText("Sign in with google is error");
+                                    return;
+                                }
+                            }
+                        });
+            } catch (ApiException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
     private void login(){
        String strEmail = email.getText().toString();
